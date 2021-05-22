d0685d80531f39da26c580837322a13c7fcd58ab
Daniel Turner <GhandiJones@users.noreply.github.com>
2021-05-21 20:32:12 -0500

    Add files via upload
    Follow up commit with mobile/desktop differentiation.

---
+++ public/js/background.js
@@ -0,0 +1,188 @@
+const canvas = document.getElementById("screen"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+// const assetsManager = new AssetsManager();
+// assetsManager.useDefaultLoadingScreen = false;
+var starPool = [];
+var backgroundStars = [];
+var density = 1000;
+var starAmount = 100;
+var checkTime = new Date();
+var timeCycle = 10;
+var cameraPOS = new BABYLON.Vector3(0, 0,-10);
+var ambientColor = new BABYLON.Color3(RandomColor(), RandomColor(), RandomColor());
+var light;
+
+
+// Add your code here matching the playground format
+
+setTimeout(function(){
+  const scene = createScene(); //Call the createScene function
+
+  // Register a render loop to repeatedly render the scene
+  engine.runRenderLoop(function () {
+          scene.render();
+  });
+
+  scene.registerBeforeRender(function(){
+      var currTime = new Date();
+      var sLength = starPool.length;
+      if(sLength !== density){
+          for(var i = 0; i < density - sLength; i+=1){
+              // Create a built-in "sphere" shape.
+              var star = BuildStar(false, scene);
+              starPool.push(star);
+          }
+      }
+
+      var newStars = [];
+      for(var i = 0; i < starPool.length; i+=1){
+          // if(i===0){
+          //     console.log(starPool[i]._children[0].material);
+          // }
+          starPool[i].time += .60;
+          // starPool[i]._children[0].material.setFloat("time", stars[i].time);
+
+          if(starPool[i].position.z > 200){
+              starPool[i].position.z += -.08;
+          } else if (starPool[i].position.z > 100){
+              starPool[i].position.z += -.12;
+          } else if (starPool[i].position.z > 20){
+              starPool[i].position.z += -.15;
+          } else {
+              starPool[i].position.z += -.18;
+          }
+
+          if(starPool[i].position.z < -10){
+              starPool[i].dispose();
+              continue;
+          }
+          newStars.push(starPool[i]);
+      }
+      starPool = newStars;
+
+      //Need to add Color lerping
+      if((currTime.getTime() - checkTime.getTime()) / 1000 > timeCycle){
+          ambientColor = new BABYLON.Color3(RandomColor(), RandomColor(), RandomColor());
+          checkTime = currTime;
+      }
+      scene.fogColor = BABYLON.Color3.Lerp(scene.fogColor, ambientColor, 0.0025);
+      light.diffuse = BABYLON.Color3.Lerp(light.diffuse, ambientColor, 0.0025);
+  });
+
+}, 4000);
+
+
+
+function createScene(){
+  const scene = new BABYLON.Scene(engine);
+  scene.clearColor = new BABYLON.Color3(.1,.1,.1);
+  scene.autoClear = true
+  // Parameters : name, position, scene
+  var camera = new BABYLON.UniversalCamera("UniversalCamera", cameraPOS, scene);
+
+  // Targets the camera to a particular position. In this case the scene origin
+  camera.setTarget(BABYLON.Vector3.Zero());
+
+  light = new BABYLON.HemisphericLight('main_light', new BABYLON.Vector3(0,0,-35), scene);
+  light.diffuse = ambientColor;
+  light.groundColor = new BABYLON.Color3(0, 0, 0);
+
+  for(var i = 0; i < density; i+=1){
+      // Create a built-in "sphere" shape.
+      var star = BuildBackgroundStar();
+      backgroundStars.push(star);
+  }
+
+  for(var i = 0; i < density; i+=1){
+      // Create a built-in "sphere" shape.
+      var star = BuildStar(true, scene);
+      starPool.push(star);
+  }
+
+  return scene;
+}
+
+
+
+
+
+
+function RandomNumberBetween(max){
+    return Math.random() * (max - -max + 1) + -max;
+}
+
+function RandomHeightDisplacement(max){
+    let runs = 5;
+    let hmap = 0;
+    for(var i = 0; i < runs; i+=1){
+        hmap += RandomNumberBetween(max);
+    }
+    return hmap / runs;
+}
+
+function RandomDepth(){
+    return Math.random() * (500 - 10 + 1) + 10;
+}
+
+function RandomColor(){
+    return Math.random();
+}
+
+function BuildStar(initial, scene){
+    var starBuffer = 200
+    var starObject = new BABYLON.Mesh("dummy", scene);
+    var star = BABYLON.MeshBuilder.CreateDisc("disc", {
+            radius: 0.25
+        }, scene);
+    starObject._color = new BABYLON.Vector3(RandomColor(), RandomColor(), RandomColor())
+    // Move the sphere upward 1/2 of its height.
+    if(initial){
+        var starPos = new BABYLON.Vector3(RandomNumberBetween($('body').width()/3), RandomNumberBetween(($('body').height() + starBuffer * 2) / 10), RandomDepth());
+    } else {
+        var starPos = new BABYLON.Vector3(RandomNumberBetween($('body').width()/3), RandomNumberBetween(($('body').height() + starBuffer * 2) / 10), 500);
+    }
+    // star.material = new BABYLON.ShaderMaterial("star_mat_" + Math.random(), scene, {
+    //         vertexElement: "vertexShaderCode",
+    //         fragmentElement: "fragmentShaderCode",
+    //     },
+    //     {
+    //         attributes: ["position", "normal", "uv", "time", "color"],
+    //         uniforms: ["world", "worldView", "worldViewProjection", "view", "projection"]
+    // });
+    starObject.time = starPos.z;
+    // star.material.setFloat("time", starObject.time);
+    // star.material.setVector3("color", starObject._color);
+    //star.material = new BABYLON.StandardMaterial("star_mat_" + Math.random(), scene)
+    //star.material.diffuseColor = color;
+    starObject.position = starPos;
+    starObject.addChild(star);
+
+    star.position = new BABYLON.Vector3.Zero;
+    starObject.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
+    return starObject;
+}
+
+function BuildBackgroundStar(scene){
+    var starBuffer = 200
+    var starObject = new BABYLON.Mesh("dummy", scene);
+    var star = BABYLON.MeshBuilder.CreateDisc("disc", {
+            radius: 0.25
+        }, scene);
+    var color = new BABYLON.Color3(RandomColor(), RandomColor(), RandomColor())
+    //Need to build better random displacement for height
+    var starPos = new BABYLON.Vector3(RandomNumberBetween(($('body').width())), RandomHeightDisplacement(($('body').height() / 100 + starBuffer * 2) / 20), 500);
+    star.material = new BABYLON.StandardMaterial("star_mat_" + Math.random(), scene)
+    star.material.diffuseColor = color;
+    starObject.position = starPos;
+    starObject.addChild(star);
+
+    star.position = new BABYLON.Vector3.Zero;
+    starObject.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
+    return starObject;
+}
+
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+        engine.resize();
+});
